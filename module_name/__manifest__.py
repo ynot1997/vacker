8b878737cf3f3e02f7dc35c933b1c4affcad09b7
Tony <tony.s@biztechnosys.com>
2022-11-05 00:07:46 +0530

    send mail and template was fixed

--- module_name/__manifest__.py
+++ module_name/__manifest__.py
@@ -20,13 +20,18 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': [
+            'base',
+            'crm',
+    ],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'data/email_template.xml',
+
     ],
     # only loaded in demonstration mode
     'demo': [
--- module_name/models/lead_notification.py
+++ module_name/models/lead_notification.py
@@ -4,33 +4,32 @@ import base64
 import datetime
 import logging
 
+import logging
+
 from odoo import api, models, fields
+
+_logger = logging.getLogger(__name__)
 
 
 class Lead(models.Model):
     _inherit = "crm.lead"
-    _description = "Lead/Opportunity"
 
     @api.model
-    def create(Lead, self, work_email):
-        rec = super(work_email, self).create(fields)
-        return rec
+    def create(self, vals):
+        user = super(Lead, self).create(vals)
+        print(user)
+        user.send_acknowledgment_email()
+        return user
 
     @api.onchange()
-    def send_acknowledgment_email(self, _logger):
+    def send_acknowledgment_email(self):
         for record in self:
             email_to = []
-            email_cc = []
-            # email_cc += self._get_sales_coordinator_head()
-            if record.target_group == 'employee':
-                template_id = self.env.ref('sales_target.email_template_target_parent_acknowledgment')
-                email_to += [record.res.users.emp_id.work_email]
 
-            email_to = self.list_to_comma_seperated(email_to)
-            email_cc = self.list_to_comma_seperated(email_cc)
-            template_id.sudo().send_mail(record.id, force_send=True,
-                                         email_values={'email_to': email_to, 'email_cc': email_cc})
-            _logger.info('Sending Email for accepting the lead for salesperson.')
-            record.is_email_sent = True
-            record.email_sent_datetime = datetime.datetime.now()
-        return super(Lead, self).send_acknowledgment_email()
+            if record.user_id:
+                template_id = self.env.ref('module_name.crm_lead_mail_template')
+                email_to += [record.user_id.work_email]
+
+                template_id.sudo().send_mail(record.id, force_send=True, email_values={'email_to': email_to})
+                _logger.info('Sending Email for accepting the lead for salesperson.')
+                
