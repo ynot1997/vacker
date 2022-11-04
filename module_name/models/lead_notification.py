2219a90db83018fb13346975e2eaa1453f34fa81
Tony <tony.s@biztechnosys.com>
2022-11-05 00:28:08 +0530

    comments were added and unwanted files are removed

--- module_name/models/lead_notification.py
+++ module_name/models/lead_notification.py
@@ -14,6 +14,7 @@ _logger = logging.getLogger(__name__)
 class Lead(models.Model):
     _inherit = "crm.lead"
 
+    # create method is used for getting function into this group
     @api.model
     def create(self, vals):
         user = super(Lead, self).create(vals)
@@ -21,6 +22,7 @@ class Lead(models.Model):
         user.send_acknowledgment_email()
         return user
 
+    # this function is to send email for the sales person which is assigned
     @api.onchange()
     def send_acknowledgment_email(self):
         for record in self:
@@ -29,7 +31,7 @@ class Lead(models.Model):
             if record.user_id:
                 template_id = self.env.ref('module_name.crm_lead_mail_template')
                 email_to += [record.user_id.work_email]
-
+                # in template the basic format of mail is set there
                 template_id.sudo().send_mail(record.id, force_send=True, email_values={'email_to': email_to})
                 _logger.info('Sending Email for accepting the lead for salesperson.')
-                
+#                 logger info for seeing the messing in the log file
